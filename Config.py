bf03e93016eeaabd31781cce8689c75e56e6d181
Arnav123ertyu <arnavma369@gmail.com>
2025-06-02 22:04:05 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = os.getenv("8061718772:AAGA86NoEREes4Ma1blRJ99yIWwyGPejOq0")
+API_ID = int(os.getenv("26627159"))
+API_HASH = os.getenv("d16873c1d14280584fac7bf4776ec8fa")
+
+MONGO_URI = os.getenv("mongodb+srv://<Arnavma369>:<Arnav123@>@cluster0.ihdrdsv.mongodb.net/")  # MongoDB URI
+SHORTENER_API = os.getenv("633210c0ef9543f9ac12c13fddbc6e2647762d54")  # e.g., https://tnshort.net/api?api=yourapikey&url=
+
+ADMINS = [123456789]  # Add your Telegram user ID
